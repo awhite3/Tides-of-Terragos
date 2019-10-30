8eb25897ae5b70633b5d0c3ee1b5754c713b6dd5
Terragos <47040288+Terragos@users.noreply.github.com>
2019-10-29 23:14:39 -0400

    Update images.js

--- images.js
+++ images.js
@@ -5,13 +5,38 @@ window.onload = function()
     {
         images.item(i).addEventListener("click", enlargePicture);
     }
-
 }
 
+/*push*/
 function enlargePicture()
 {
+    anyAreBig = false;
+    var images = document.getElementsByTagName('img');
+    for(var i = 0; i < images.length; i++)
+    {
+        if(images.item(i).width == 500){
+            anyAreBig = true;
+        }
+    }
+
+
     if(this.width == 500){
-        this.width = 280;    
+
+        var images = document.getElementsByTagName('img');
+        for(var i = 0; i < images.length; i++)
+        {
+            images.item(i).width = 280;
+        } 
+
+    } else if(this.width == 280 && anyAreBig){
+
+        var images = document.getElementsByTagName('img');
+        for(var i = 0; i < images.length; i++)
+        {
+            images.item(i).width = 280;
+        } 
+        this.width = 500;
+
     } else {
         this.width = 500;
     }
